797b945c69cdac1522b434c7ceff575491a7d990
Jac <u_jar@sina.com>
2016-06-13 14:18:06 +0800

    1. add browser website feature

--- app/src/main/java/com/peanutswifi/BrowserWebsiteLauncher.java
+++ app/src/main/java/com/peanutswifi/BrowserWebsiteLauncher.java
@@ -1,6 +1,10 @@
 package com.peanutswifi;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,16 +12,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 
 //http://www.cnblogs.com/mengdd/p/3142442.html
@@ -29,10 +31,9 @@ public class BrowserWebsiteLauncher extends Activity {
     private String TAG = "http";
     private EditText mURLEdit = null;
 
-    private Button mBrowserButton = null;
-    private TextView mResultText = null;
-
-    private String URL = null;
+    public Button mBrowserButton = null;
+    private TextView mHeaderText = null;
+    private TextView mContentsText = null;
 
     /** Called when the activity is first created. */
     @Override
@@ -48,70 +49,107 @@ public class BrowserWebsiteLauncher extends Activity {
     private void bindListeners() {
         mBrowserButton = (Button) findViewById(R.id.browserBtn);
         mURLEdit = (EditText) findViewById(R.id.urlEdit);
-        mResultText = (TextView) findViewById(R.id.resultText);
+        mHeaderText = (TextView) findViewById(R.id.headerText);
+        mContentsText = (TextView) findViewById(R.id.contentsText);
 
         mBrowserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
                 mBrowserButton.setEnabled(false);
-                Log.i(TAG, "GET request");
-//                获取要访问的url
-                URL = mURLEdit.getText().toString();
-                // 生成请求对象
-                if (URL != "") {
-                    HttpGet httpGet = new HttpGet(URL);
-                    HttpClient httpClient = new DefaultHttpClient();
-                    // 发送请求
-                    try {
-
-                        HttpResponse response = httpClient.execute(httpGet);
-
-                        // 显示响应
-                        showResponseResult(response);// 一个私有方法，将响应结果显示出来
-
-                    }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
-                    }
+                Log.d(TAG, "GET request");
+                //                获取要访问的url
+                // Gets the URL from the UI's text field.
+                String stringUrl = mURLEdit.getText().toString();
+                ConnectivityManager connMgr = (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    new DownloadWebpageTask().execute(stringUrl);
+                } else {
+                    mHeaderText.setText("No network connection available.");
                 }
+                mBrowserButton.setEnabled(true);
             }
         });
     }
 
-    /**
-     * 显示响应结果到命令行和TextView
-     * @param response
-     */
-    private void showResponseResult(HttpResponse response)
-    {
-        if (null == response)
-        {
-            return;
-        }
+    // Uses AsyncTask to create a task away from the main UI thread. This task takes a
+    // URL string and uses it to create an HttpUrlConnection. Once the connection
+    // has been established, the AsyncTask downloads the contents of the webpage as
+    // an InputStream. Finally, the InputStream is converted into a string, which is
+    // displayed in the UI by the AsyncTask's onPostExecute method.
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String[]> {
+        @Override
+        protected String[] doInBackground(String... urls) {
 
-        HttpEntity httpEntity = response.getEntity();
-        try
-        {
-            InputStream inputStream = httpEntity.getContent();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    inputStream));
-            String result = "";
-            String line = "";
-            while (null != (line = reader.readLine()))
-            {
-                result += line;
-
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                String[] error = {"Unable to retrieve web page. URL may be invalid.",
+                        "null"};
+                return error;
             }
-
-            System.out.println(result);
-            mResultText.setText("Response Content from server: " + result);
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String[] result) {
+            mHeaderText.setText(result[0]);
+            mContentsText.setText(result[1]);
+        }
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+    private String[] downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        String resultHeader = "==============header=============\n";
+        String resultContents = "\n==============contents=============\n";
+        String[] resultAll = new String[2];
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(TAG, "The response is: " + response);
+
+            Map<String, List<String>> headerMap = conn.getHeaderFields();
+            resultHeader = resultHeader + headerMap.toString();
+
+            is = conn.getInputStream();
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is);
+            resultContents = resultContents + contentAsString;
+
+            resultAll[0] = resultHeader;
+            resultAll[1] = resultContents;
+            return resultAll;
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
         }
 
     }
 
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream) throws IOException {
+        Reader reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[4096];
+        StringBuilder builder = new StringBuilder();
+        int len;
+        while ((len = reader.read(buffer)) > 0) {
+            builder.append(buffer, 0, len);
+        }
+        return builder.toString();
+    }
 }
